ad1ba884a49f73268d8c2a07d6810ed94c264824
AkshayThiru <akshay_t@berkeley.edu>
2025-06-06 11:04:14 -0700

    Add file I/O using the Feather library

---
+++ src/file_io.cc
@@ -0,0 +1,110 @@
+#include "dgd_benchmark/file_io.h"
+
+#include <arrow/api.h>
+#include <arrow/io/api.h>
+#include <arrow/ipc/feather.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "dgd_benchmark/benchmark_result.h"
+
+namespace {
+
+arrow::Result<std::shared_ptr<arrow::Array>> MakeDoubleArray(
+    const std::vector<double>& data) {
+  arrow::DoubleBuilder builder;
+  ARROW_RETURN_NOT_OK(builder.AppendValues(data.data(), data.size()));
+  std::shared_ptr<arrow::Array> array;
+  ARROW_RETURN_NOT_OK(builder.Finish(&array));
+  return array;
+}
+
+// Assumes int maps to arrow::int32().
+arrow::Result<std::shared_ptr<arrow::Array>> MakeInt32Array(
+    const std::vector<int>& data) {
+  arrow::Int32Builder builder;
+  ARROW_RETURN_NOT_OK(builder.AppendValues(data.data(), data.size()));
+  std::shared_ptr<arrow::Array> array;
+  ARROW_RETURN_NOT_OK(builder.Finish(&array));
+  return array;
+}
+
+// Specialization for std::vector<bool> because of its packed nature.
+arrow::Result<std::shared_ptr<arrow::Array>> MakeBooleanArray(
+    const std::vector<bool>& data) {
+  arrow::BooleanBuilder builder;
+  ARROW_RETURN_NOT_OK(builder.Reserve(data.size()));
+  for (bool val : data) {
+    ARROW_RETURN_NOT_OK(builder.Append(val));
+  }
+  std::shared_ptr<arrow::Array> array;
+  ARROW_RETURN_NOT_OK(builder.Finish(&array));
+  return array;
+}
+
+inline arrow::Status SaveBenchmarkOutputToFeather(
+    const BenchmarkResultArray& resa, const std::string& filename) {
+  std::shared_ptr<arrow::Schema> schema =
+      arrow::schema({arrow::field("solve_time", arrow::float64()),
+                     arrow::field("prim_dual_gap", arrow::float64()),
+                     arrow::field("prim_feas_err", arrow::float64()),
+                     arrow::field("dual_feas_err", arrow::float64()),
+                     arrow::field("iter", arrow::int32()),
+                     arrow::field("optimal", arrow::boolean())});
+
+  std::shared_ptr<arrow::Array> solve_time_array;
+  ARROW_ASSIGN_OR_RAISE(solve_time_array, MakeDoubleArray(resa.solve_time));
+
+  std::shared_ptr<arrow::Array> prim_dual_gap_array;
+  ARROW_ASSIGN_OR_RAISE(prim_dual_gap_array,
+                        MakeDoubleArray(resa.prim_dual_gap));
+
+  std::shared_ptr<arrow::Array> prim_feas_err_array;
+  ARROW_ASSIGN_OR_RAISE(prim_feas_err_array,
+                        MakeDoubleArray(resa.prim_feas_err));
+
+  std::shared_ptr<arrow::Array> dual_feas_err_array;
+  ARROW_ASSIGN_OR_RAISE(dual_feas_err_array,
+                        MakeDoubleArray(resa.dual_feas_err));
+
+  std::shared_ptr<arrow::Array> iter_array;
+  ARROW_ASSIGN_OR_RAISE(iter_array, MakeInt32Array(resa.iter));
+
+  std::shared_ptr<arrow::Array> optimal_flag_array;
+  ARROW_ASSIGN_OR_RAISE(optimal_flag_array,
+                        MakeBooleanArray(resa.optimal_flag));
+
+  std::shared_ptr<arrow::Table> table{
+      arrow::Table::Make(schema, {
+                                     solve_time_array,
+                                     prim_dual_gap_array,
+                                     prim_feas_err_array,
+                                     dual_feas_err_array,
+                                     iter_array,
+                                     optimal_flag_array,
+                                 })};
+
+  std::shared_ptr<arrow::io::FileOutputStream> file;
+  ARROW_ASSIGN_OR_RAISE(file, arrow::io::FileOutputStream::Open(filename));
+
+  ARROW_RETURN_NOT_OK(arrow::ipc::feather::WriteTable(*table, file.get()));
+
+  ARROW_RETURN_NOT_OK(file->Close());
+  return arrow::Status::OK();
+}
+
+}  // namespace
+
+bool SaveBenchmarkOutputToFile(const BenchmarkResultArray& out,
+                               const std::string& filename) {
+  arrow::Status st{SaveBenchmarkOutputToFeather(out, filename)};
+  if (!st.ok()) {
+    std::cerr << "Failed to write Feather file: " << st.ToString() << std::endl;
+    return false;
+  }
+
+  std::cout << "Benchmark data written to: " << filename << std::endl;
+  return true;
+}
